15786744df5d4069eafad65378fb9f8a4469101c
Ruben Marques <rubenrfhm98@gmail.com>
2025-05-12 17:22:21 +0100

    Move SAAS starter into main

--- apps/www/src/config/clone-cmd.ts
+++
@@ -1,7 +0,0 @@
-export const cloneCmd = {
-    starterkits: {
-        saasNextjs: {
-            base: 'create-next-app@latest my-saas-app \ --example "https://github.com/alifarooq9/launchmvpfast/tree/main/starterkits/saas"',
-        },
-    },
-}
